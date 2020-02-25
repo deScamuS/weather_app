5acbeb0f45878b73763dbcbcb1e7dd94a1f9ebba
ajax <ajax@Tys-iMac.local>
2020-02-24 21:26:30 -0600

    fetched data to console

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react"
+import WeatherQuery from "./components/WeatherQuery"
+const API_KEY = "168c5cb818e74abd926a9d65d285d48f"
 
-export default class App extends Component {
-  const
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      city: ""
+    }
+  }
+  componentDidMount() {
+    fetch(
+      "https://api.weatherbit.io/v2.0/forecast/daily?city=Seattle,WA&key=168c5cb818e74abd926a9d65d285d48f"
+    )
+      .then(res => res.json())
+      .then(data => this.setState({ response: data }))
+  }
+
   render() {
     return (
-      <div className='App'>
-        
+      <div className="container">
+        <WeatherQuery />
       </div>
     )
   }
 }
+export default App
---
+++ src/components/WeatherQuery.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export const WeatherQuery = () => {
+  return (
+    <div className='weather-query'>    
+    
+      <form>
+        <input type="text" value="" placeholder="" />
+        <button type="search"></button>
+      </form>
+    </div>
+  )
+}
+
+export default WeatherQuery;
