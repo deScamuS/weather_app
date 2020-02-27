0406c572f652b2a8c9a4cb410eb768f7dcb7552d
ajax <ajax@Tys-iMac.local>
2020-02-27 11:51:27 -0600

    Updates

---
+++ precache-manifest.4221301383f099d2bdebceadb39852d7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3035e57697de0b7dc6772c40645469",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "a555dcb9f937ef40582f",
+    "url": "/weather_app/static/css/2.f6939e2d.chunk.css"
+  },
+  {
+    "revision": "21d4856b303190e99d60",
+    "url": "/weather_app/static/css/main.ec53f59a.chunk.css"
+  },
+  {
+    "revision": "a555dcb9f937ef40582f",
+    "url": "/weather_app/static/js/2.062c8289.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather_app/static/js/2.062c8289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21d4856b303190e99d60",
+    "url": "/weather_app/static/js/main.0eca639b.chunk.js"
+  },
+  {
+    "revision": "4114478f37f38f0b4000",
+    "url": "/weather_app/static/js/runtime-main.edaf372d.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/weather_app/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/weather_app/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/weather_app/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/weather_app/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/weather_app/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/weather_app/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/weather_app/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/weather_app/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/weather_app/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/weather_app/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/weather_app/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/weather_app/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/weather_app/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/weather_app/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/weather_app/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/weather_app/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/weather_app/static/media/fa-solid-900.cdfec5cf.woff"
+  }
+]);
